20ab432b78d6fd5d4003a501ae0dc7e0251cda63
Umeozulu Anthony <tonyumeozulu@gmail.com>
2024-04-04 21:09:57 +0100

    home progress

---
+++ src/components/CategoryListing.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+
+type Props = {
+  image: string
+  title: string
+  link: string
+  bottom?: boolean
+}
+
+const CategoryListing = ({ bottom, image, link, title }: Props) => {
+  return (
+    <div className="snap-start h-60 relative mr-[15px]">
+      <div className="h-60 relative w-60 rounded-[5px] bg-white">
+        <img
+          src={image}
+          className={`h-full object-cover object-top w-full rounded-[5px] ${
+            bottom ? "object-cover object-bottom" : ""
+          }`}
+          alt="img"
+        />
+        <Link to={link}>
+          <p className="border border-malon-color text-malon-color cursor-pointer flex justify-center absolute capitalize w-[170px] rounded-[5px] border-solid right-0 bottom-0">
+            {title}
+          </p>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default CategoryListing
---
+++ src/components/Product.tsx
@@ -0,0 +1,111 @@
+import { useMemo, useState } from "react"
+import { IProduct } from "../types/product"
+import { Link } from "react-router-dom"
+import { FaArrowsAlt, FaHeart, FaThumbsUp } from "react-icons/fa"
+import Model from "./Model"
+
+type Props = {
+  product: IProduct
+}
+
+const Product = ({ product }: Props) => {
+  const [showModel, setShowModel] = useState(false)
+
+  const discount = useMemo(() => {
+    if (!product.costPrice || product.sellingPrice) {
+      return null
+    }
+    if (product.costPrice < product.sellingPrice) {
+      return null
+    }
+
+    return (
+      ((product.costPrice - product.sellingPrice) / product.costPrice) * 100
+    )
+  }, [product.costPrice, product.sellingPrice])
+
+  return (
+    <div className="h-[342px] w-[162px] mr-2.5 mb-2.5 lg:h-[500px] lg:w-60 lg:flex-[0_0_auto]">
+      {/* {showNotification && (
+        <Notification text="Item added to Cart" buttonText={"Checkout"} />
+      )} */}
+      <Model showModel={showModel} setShowModel={setShowModel}>
+        <div className="flex justify-center h-full">
+          <img
+            className="w-auto h-full"
+            src={product.images[0]}
+            alt={product.name}
+          />
+        </div>
+      </Model>
+
+      <div className="h-[270px] lg:h-[400px] overflow-hidden relative w-full">
+        <Link to={`/product/${product.slug}`}>
+          {product.sold ? (
+            <div className="absolute text-[white] uppercase p-[5px] rounded-[5px] right-2.5 top-2.5">
+              sold
+            </div>
+          ) : (
+            discount && (
+              <span className="discount-tag1 ">{discount.toFixed(0)}% off</span>
+            )
+          )}
+
+          <img
+            src={product.images[0]}
+            className="h-[270px] object-cover w-full lg:h-[400px]"
+            alt={product.name}
+          />
+        </Link>
+
+        <ul className="absolute text-center w-full z-[3] left-0 bottom-[30px]">
+          <li className="inline-block opacity-0 relative mr-2.5 top-[100px] list-none transition-all duration-[0.4s] ease-[ease] delay-[0.15s]">
+            <span
+              className="bg-white shadow-[0_0_25px_hsla(0,0%,9%,0.4)] text-black block text-lg h-[45px] leading-[45px] transition-all duration-500 w-[45px] rounded-[50px]"
+              onClick={() => setShowModel(!showModel)}
+            >
+              <FaArrowsAlt className="text-orange-color inline-block relative rotate-[0] transition-all duration-[0.3s]" />
+            </span>
+          </li>
+          <li className="inline-block opacity-0 relative mr-2.5 top-[100px] list-none transition-all duration-[0.4s] ease-[ease] delay-[0.15s]">
+            <span
+              className="bg-white shadow-[0_0_25px_hsla(0,0%,9%,0.4)] text-black block text-lg h-[45px] leading-[45px] transition-all duration-500 w-[45px] rounded-[50px]"
+              // onClick={toggleLikes}
+            >
+              <FaThumbsUp className="text-orange-color inline-block relative rotate-[0] transition-all duration-[0.3s]" />
+            </span>
+          </li>
+          <li className="inline-block opacity-0 relative top-[100px] list-none transition-all duration-[0.4s] ease-[ease] delay-[0.2s] mr-0">
+            <span
+              className="bg-white shadow-[0_0_25px_hsla(0,0%,9%,0.4)] text-black block text-lg h-[45px] leading-[45px] transition-all duration-500 w-[45px] rounded-[50px]"
+              // onClick={() => saveItem()}
+            >
+              <FaHeart className="text-orange-color inline-block relative rotate-[0] transition-all duration-[0.3s]" />
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div className="max-h-[100px] w-full pt-2.5">
+        <h2 className="text-xl capitalize font-bold overflow-hidden text-ellipsis whitespace-nowrap w-full mb-0">
+          <Link to={`/product/${product.slug}`}>{product.name}</Link>
+        </h2>
+        <p className="text-xs h-4 leading-5 opacity-50 overflow-hidden capitalize w-full lg:text-base mb-[5px]">
+          {product.brand}
+        </p>
+        <span className="text-base lg:text-xl font-black">
+          {/* {product.currency} */}
+          {product.sellingPrice}
+        </span>
+        {product.costPrice &&
+          (product.costPrice > product.sellingPrice ? (
+            <span className="text-base ml-5 line-through text-orange-color">
+              {/* {product.currency} */}
+              {product.costPrice}
+            </span>
+          ) : null)}
+      </div>
+    </div>
+  )
+}
+
+export default Product
