6458f66320238495ab05ae8ab1be2cee6bb53d1a
Umeozulu Anthony <tonyumeozulu@gmail.com>
2024-04-08 19:41:03 +0100

    footer progress

--- src/pages/defaults/index.tsx
+++ src/pages/defaults/index.tsx
@@ -1,13 +1,15 @@
-import { Outlet } from 'react-router-dom';
-import Navbar from '../../components/layout/navbar';
+import { Outlet } from "react-router-dom"
+import Navbar from "../../components/layout/navbar"
+import Footer from "../../components/layout/footer"
 
 function Default() {
   return (
     <main className="">
       <Navbar />
       <Outlet />
+      <Footer />
     </main>
-  );
+  )
 }
 
-export default Default;
+export default Default
