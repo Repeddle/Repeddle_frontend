f14d7bef8f77fe9a7116b3486914f62bacf69d8f
Umeozulu Anthony <tonyumeozulu@gmail.com>
2024-07-12 09:58:47 +0100

    changes

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -2,10 +2,9 @@ import { Navigate, Outlet, useSearchParams } from "react-router-dom"
 import useAuth from "../../hooks/useAuth"
 import { useMemo } from "react"
 import AuthNav from "../../components/layout/AuthNav"
-import LoadingPage from "../../components/ui/LoadingPage"
 
 export default function Auth() {
-  const { loading, user } = useAuth()
+  const { user } = useAuth()
   const [searchParam] = useSearchParams()
   const redirectUrl = useMemo(() => searchParam.get("redirect"), [searchParam])
 
@@ -17,7 +16,6 @@ export default function Auth() {
     <div>
       <AuthNav />
       <Outlet />
-      {loading && <LoadingPage />}
     </div>
   )
 }
